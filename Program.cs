7bc5fe720353e449d481a495f969e7ef273ee709
Sabarish Nair <121101278@umail.ucc.ie>
2022-06-05 13:41:47 +0100

    Updated

--- Program.cs
+++ Program.cs
@@ -15,11 +15,10 @@ builder.Services.AddScoped<IPrescriptiveSolver, PrescriptiveSolver>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+
 
 app.UseHttpsRedirection();
 
